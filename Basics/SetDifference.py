52d8cb3a49c525d37b7ef10c46411505980e122a
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-19 13:31:34 +0530

    [Sudhanshu] Add: Added python program to print the set difference

---
+++ Basics/SetDifference.py
@@ -0,0 +1,18 @@
+'''
+
+    @Author: Sudhanshu Kumar
+    @Date: 19-09-2024
+    @Last Modified by: Sudhanshu Kumar
+    @Last Modified time: 19-09-2024
+    @Title : Write a Python program to print the difference between two lists
+
+'''
+
+
+def main():
+    set1 = {'Math','physics','comp','history'}
+    set2 = {'art','history'}
+    print(set1.difference(set2))
+
+if __name__=="__main__":
+    main()
