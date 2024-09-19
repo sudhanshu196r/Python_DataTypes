8d50ed3aeba85ee7603b5241a89e7ff68ed74202
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-19 13:26:19 +0530

    [Sudhanshu] Add: Added python program to print the first and last colour from a list

---
+++ Basics/FirstLastColor.py
@@ -0,0 +1,17 @@
+'''
+
+    @Author: Sudhanshu Kumar
+    @Date: 18-09-2024
+    @Last Modified by: Sudhanshu Kumar
+    @Last Modified time: 18-09-2024
+    @Title : Write a Python program to print first and last colour from a list of colours
+
+'''
+
+def main():
+    color_list = ["white","green","red","black"]
+    print(f"First Colour is: {color_list[0]} \nLast Colour is: {color_list[-1]}")
+
+if __name__=="__main__":
+    main()
+
