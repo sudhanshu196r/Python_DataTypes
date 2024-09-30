be67687e5406c8ce3c8d76d09930db3befae16a6
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-30 16:41:51 +0530

    [sudhanshu] Add: Added a python file to count occurance of substring inside a string

---
+++ String_Problems/SubstringOccurence.py
@@ -0,0 +1,33 @@
+'''
+
+    @Author: Sudhanshu Kumar
+    @Date: 30-09-2024
+    @Last Modified by: Sudhanshu Kumar
+    @Last Modified time: 30-09-2024
+    @Title : Python program to print formatted string with width 50
+
+'''
+
+def countSub(string,sub):
+    """
+    Description: 
+        This method will return the count of substring in the string
+
+    Parameters:
+        string: in which we have to  look for the substring
+        sub: substring that we have to count
+    
+    Returns:
+        returns count of substring
+    """
+
+
+    return string.count(sub)
+
+def main():
+    string = 'sudhanshu'
+    print(countSub(string,'u'))
+
+
+if __name__=="__main__":
+    main()
