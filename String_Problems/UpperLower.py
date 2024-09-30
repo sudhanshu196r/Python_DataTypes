90a53b6f93c4de15b480d25089a5b988c8318ea8
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-30 15:35:19 +0530

    [sudhanshu] Add: Added a python file to take user input and convert that to upper and lower case

---
+++ String_Problems/UpperLower.py
@@ -0,0 +1,17 @@
+'''
+
+    @Author: Sudhanshu Kumar
+    @Date: 30-09-2024
+    @Last Modified by: Sudhanshu Kumar
+    @Last Modified time: 30-09-2024
+    @Title : Python program to add find the longest word from a list of words
+
+'''
+
+def main():
+    word = input("Enter a word : ")
+    print(word.upper())
+    print(word.lower())
+
+if __name__=="__main__":
+    main()
