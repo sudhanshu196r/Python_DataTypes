bda8a550b4cbdc0dc416a1a6766c38c01147ab4e
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-30 12:06:41 +0530

    [sudhanshu] Add: Added a python file to calculate the length of the string

---
+++ String_Problems/StringLength.py
@@ -0,0 +1,27 @@
+'''
+
+    @Author: Sudhanshu Kumar
+    @Date: 25-09-2024
+    @Last Modified by: Sudhanshu Kumar
+    @Last Modified time: 25-09-2024
+    @Title : Python program to get sum of elements of list
+
+'''
+
+def strlength(str):
+    """
+    Description:
+        This method will find the length of the string
+    Parameters:
+        It takes string as a parameter
+    Returns:
+        It returns length of the string
+    """
+    return len(str)
+
+def main():
+    str = "sudhanshu kumar"
+    print(f"Length of string is {strlength(str)}")
+
+if __name__=="__main__":
+    main()
